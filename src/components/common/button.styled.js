7efddec97de66c0459904e5993618e82afa5366e
Yoruyeh <orchardsky31@gmail.com>
2023-06-05 14:49:47 +0800

    refactor: button styles

--- src/components/common/button.styled.js
+++ src/components/common/button.styled.js
@@ -1,33 +1,58 @@
 import styled, { css } from 'styled-components';
 
-const StyledButton = styled.button`
+const StyledAuthButton = styled.button`
   border-radius: 50px;
   background-color: #ff6600;
   border: none;
   color: white;
-  width: ${({ width }) => (width ? width : '356px')};
-  height: ${({ height }) => (height ? height : '46px')};
+  width: 356px;
+  height: 46px;
   font-family: 'Noto Sans TC', sans-serif;
-  font-weight: bold;
-  padding: 8px 24px;
+  font-size: 20px;
+  font-weight: regular;
+  padding: 8px 0;
   margin: 8px 0;
+  text-align: center;
   white-space: nowrap;
   &:hover {
     cursor: pointer;
   }
+`;
+
+const StyledNavbarButton = styled(StyledAuthButton)`
+  width: 178px;
+`
+
+const StyledSaveButton = styled(StyledAuthButton)`
+  width: 88px;
+  heigth: 64px;
+`
+
+const StyledFollowButton = styled(StyledAuthButton)`
+  width: 96px;
+  height: 40px;
+  font-size: 16px;
   ${({ isUnfollow }) =>
     isUnfollow &&
     css`
       background-color: #ffffff;
       color: #ff6600;
       border: 1px solid #ff6600;
-      border-radius: 50px;
       width: 64px;
       height: 40px;
-      font-size: 16px;
-      font-weight: normal;
-      padding: 8px 16px;
     `}
-`;
+`
 
-export default StyledButton;
+const StyledInputButton = styled(StyledAuthButton)`
+  width: 64px;
+  heigth: 40px;
+  font-size: 16px;
+`
+
+export {
+  StyledAuthButton as AuthButton,
+  StyledNavbarButton as NavbarButton,
+  StyledSaveButton as SaveButton,
+  StyledFollowButton as FollowButton,
+  StyledInputButton as InputButton,
+};
