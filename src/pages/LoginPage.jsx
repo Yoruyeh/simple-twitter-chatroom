f5c931010d14cc326cb114f2128ee82944e8cc42
Liam <liam67726978@gmail.com>
2023-06-06 14:24:24 +0800

    feat: create LoginPage

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import Login from '../components/Login'
+import Layout from '../layout/Layout'
+
+export default function LoginPage() {
+  return (
+    <Layout className='justify-content-center'>
+      <Login />
+    </Layout>
+  )
+}
