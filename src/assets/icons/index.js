07a363b01dfd40fef8ec71445df545fecbb15ac4
Yoruyeh <orchardsky31@gmail.com>
2023-06-06 21:33:24 +0800

    doc: add icons to assets file

--- src/assets/icons/index.js
+++
@@ -1,8 +0,0 @@
-import { ReactComponent as LogoIcon } from './logo.svg';
-import { ReactComponent as HomeIcon } from './home.svg';
-import { ReactComponent as UserIcon } from './user.svg';
-import { ReactComponent as SettingIcon } from './setting.svg';
-import { ReactComponent as LogoutIcon } from './logout.svg';
-import { ReactComponent as LikeIcon } from './like.svg';
-import { ReactComponent as ReplyeIcon } from './reply.svg';
-export { LogoIcon, HomeIcon, UserIcon, SettingIcon, LogoutIcon, LikeIcon, ReplyeIcon };
