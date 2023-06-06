8513bc280e899f5ec841cfb6744df14a38ba861a
Liam <liam67726978@gmail.com>
2023-06-06 16:27:55 +0800

    feat: create colorPalettes styles

---
+++ src/components/common/colorPalette.js
@@ -0,0 +1,29 @@
+// 品牌色
+const logoColor = '#FF6600' // Logo顏色
+
+// 主要顏色
+const primaryColor = '#0062FF' // Primary
+const secondaryColor = '#6C757D' // Secondary
+const successColor = '#3DD598' // Success
+const warningColor = '#FFC542' // Warning
+const errorColor = '#FC5A5A' // Error
+
+// 輔助色
+const orangeColor = '#FF974A' // 橘色
+const blueColor = '#50B5FF' // 藍色
+const greenColor = '#82C43C' // 綠色
+const purpleColor = '#A461D8' // 紫色
+const pinkColor = '#FF9AD5' // 粉色
+
+// 灰階色
+const black = '#000000' // 黑色
+const gray1 = '#1A1A1A' // 深灰色1
+const gray2 = '#333333' // 深灰色2
+const gray3 = '#4D4D4D' // 深灰色3
+const gray4 = '#666666' // 中灰色1
+const gray5 = '#808080' // 中灰色2
+const gray6 = '#999999' // 中灰色3
+const gray7 = '#B3B3B3' // 浅灰色1
+const gray8 = '#CCCCCC' // 浅灰色2
+const gray9 = '#E6E6E6' // 浅灰色3
+const white = '#FFFFFF' // 白色
