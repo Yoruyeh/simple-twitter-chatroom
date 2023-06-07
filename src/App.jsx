21052579522a35f30672a733a79f6ce4aafe1059
Liam <liam67726978@gmail.com>
2023-06-07 18:49:28 +0800

    refactor: add strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
 import { useRoutes } from 'react-router-dom'
 import routes from './routes'
 
 export default function App() {
   const element = useRoutes(routes)
-  return <>{element}</>
+  return <React.StrictMode>{element}</React.StrictMode>
 }
