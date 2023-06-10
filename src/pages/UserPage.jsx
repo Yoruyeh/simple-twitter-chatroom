7b0433bfc8e02a0237bb54b713196e5b52f0e8d5
Liam <liam67726978@gmail.com>
2023-06-10 17:11:08 +0800

    refactor: Update UserPage、UserInfoCard 、 MainLayout、Tab.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,60 +1,28 @@
 import styled from 'styled-components'
 import { UserInfoCard } from '../components/common/UserInfoCard'
-import Navbar from '../components/Navbar'
-import {
-  PopularFollower,
-  PopularFollowerItem,
-} from '../components/PopularFollower'
 import Tab from '../components/common/Tab'
+import MainLayout from '../layout/MainLayout'
+import { UserHeader } from '../components/Header'
 
 const StyledContainer = styled.div`
-  .main-container {
-    width: 639px;
-  }
-  .main-info {
+  .user-info {
     margin-bottom: 16px;
   }
-  @media screen and (min-width: 992px) and (max-width: 1199px) {
-    max-width: 960px;
-  }
-
-  @media screen and (min-width: 1200px) {
-    max-width: 1140px;
-  }
 `
-
 export default function UserPage() {
   return (
-    <StyledContainer className='container-fluid px-0'>
-      <div className='row'>
-        <div className='col-2 px-0'>
-          <Navbar />
+    <MainLayout>
+      <StyledContainer className='container-fuild'>
+        <div className='header'>
+          <UserHeader />
         </div>
-
-        <div className='col-7 px-0'>
-          <div className='container-fluid px-0 main-container'>
-            <div className='row'>
-              <div className='col-12 main-header'>
-                <h5>= =</h5>
-              </div>
-              <div className='col-12 main-info'>
-                <UserInfoCard />
-              </div>
-              <div className='col-12 main-tab '>
-                <Tab></Tab>
-              </div>
-            </div>
-          </div>
+        <div className='user-info'>
+          <UserInfoCard />
         </div>
-
-        <div className='col-3 px-0'>
-          <PopularFollower>
-            <PopularFollowerItem />
-            <PopularFollowerItem />
-            <PopularFollowerItem />
-          </PopularFollower>
+        <div className='user-tab'>
+          <Tab></Tab>
         </div>
-      </div>
-    </StyledContainer>
+      </StyledContainer>
+    </MainLayout>
   )
 }
