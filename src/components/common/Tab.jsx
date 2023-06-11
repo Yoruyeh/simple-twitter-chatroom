67b918652761f34fb38de98975c8e384c35d621b
Liam <liam67726978@gmail.com>
2023-06-12 03:16:00 +0800

    refactor: Update Tab

--- src/components/common/Tab.jsx
+++ src/components/common/Tab.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
-import { useNavigate } from 'react-router-dom' 
-
+import { useState, useEffect } from 'react'
+import { useNavigate, NavLink, Outlet } from 'react-router-dom'
+import { getTweetById } from '../../api/tweets'
+import { checkPermission } from '../../api/checkPermission'
 // 顏色變量
 const dividerColor = '#E6ECF0'
 const fontDefaultColor = '#657786'
@@ -13,22 +15,20 @@ const StyledContainer = styled.div`
 
   color: ${fontDefaultColor};
   border-bottom: solid 1px ${dividerColor};
-`
 
-// Item
-const Item = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  border: none;
-  background-color: #fff;
-  font-size: 15px;
-  width: 130px;
-  height: 52px;
-  z-index: 999;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &:hover {
+    color: #657786;
+    font-size: 15px;
+    width: 130px;
+    max-width: 130px;
+    height: 52px;
+    text-decoration: none;
+  }
+  .active {
     border-bottom: 2px solid var(--main);
     color: var(--main);
   }
@@ -37,33 +37,104 @@ const Item = styled.button`
 // 根據條件返回相應的內容
 function ReturnItems() {
   const navigate = useNavigate()
-  
-  if (window.location.pathname.includes('followers') || window.location.pathname.includes('followings')) {
+  function computedClassName({ isActive }) {
+    return isActive ? 'active' : ''
+  }
+
+  if (
+    window.location.pathname.includes('followers') ||
+    window.location.pathname.includes('followings')
+  ) {
     return (
       <>
-      {/* id還要改 */}
-        <Item className='col-4' onClick={() => navigate('/14/followers')}>追蹤者</Item>
-        <Item className='col-4' onClick={() => navigate('/14/followings')}>正在追蹤</Item>
-        <Item className='col-4'></Item>
+        <li>
+          <NavLink
+            className={computedClassName}
+            onClick={() => navigate('/14/followers')}
+          >
+            追蹤者
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={computedClassName}
+            onClick={() => navigate('/14/followings')}
+          >
+            正在追蹤
+          </NavLink>
+        </li>
+        <li>
+          <NavLink></NavLink>
+        </li>
       </>
     )
   } else {
     return (
       <>
-        <Item className='col-4'>推文</Item>
-        <Item className='col-4'>回覆</Item>
-        <Item className='col-4'>喜歡的內容</Item>
+        <li>
+          <NavLink className={computedClassName} to='/userid' end>
+            推文
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={computedClassName} to='replies'>
+            回覆
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={computedClassName} to='likes'>
+            喜歡的內容
+          </NavLink>
+        </li>
       </>
     )
   }
 }
 
 export default function Tab() {
+  const [tweets, setTweets] = useState({})
+  const navigate = useNavigate()
+  useEffect(() => {
+    let userId = ''
+
+    // 取得推文函數
+    async function getTweetByidAsync(userId) {
+      const tweets = await getTweetById(userId)
+      console.log(tweets)
+      setTweets(tweets)
+    }
+    // 檢查 token 是否有效、並取得使用者資料
+    async function checkTokenIsValid() {
+      // 在本地瀏覽器取得 token
+      const token = localStorage.getItem('token')
+      // token 不存在，跳轉到 login
+      if (!token) {
+        navigate('/login')
+      }
+      // 驗證 token 是否有效
+      // 無效則返回 login
+      const result = await checkPermission(token)
+      if (!result) {
+        navigate('/login')
+      }
+      // 取得 id
+      userId = result.id
+      // 取得使用者資料
+      // getUserASync(token, userId)
+      console.log(userId)
+      getTweetByidAsync()
+    }
+    checkTokenIsValid()
+  }, [navigate, tweets])
   return (
     <>
       <StyledContainer className='container-fluid'>
-        <div className='d-flex'>
+        <ul className='d-flex link-wrapper'>
           <ReturnItems />
+        </ul>
+        <div className='tweet-wrapper'>
+          {/* 嵌套路由組件呈現的位置 */}
+          <Outlet></Outlet>
         </div>
       </StyledContainer>
     </>
