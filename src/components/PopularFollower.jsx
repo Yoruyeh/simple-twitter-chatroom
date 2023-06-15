5aa70bb2a97a1b6a025b41143e169f3b75faeee3
Yoruyeh <orchardsky31@gmail.com>
2023-06-15 15:10:10 +0800

    style: fix follow style

--- src/components/PopularFollower.jsx
+++ src/components/PopularFollower.jsx
@@ -27,12 +27,19 @@ const StyledFollowerContainer = styled.div`
 `;
 
 const StyledFollowerItem = styled.div`
-  width: 273px;
+  width: 100%;
   height: 82px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 16px;
+
+  .popular-button-wrapper {
+    width: 50%;
+    height: 100%
+    display: flex;
+    text-align: center;
+  }
 `;
 const StyledFollowerAvatar = styled.div`
   width: 50px;
@@ -43,13 +50,20 @@ const StyledFollowerAvatar = styled.div`
 `;
 const StyledFollowerInfo = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-around;
+  width: 50%;
+  height: 100%;
+  overflow:hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
   .popular-follower-name {
     line-height: 26px;
   }
   .popular-follower-account {
     color: var(--dark-70);
+    font-weight: 500;
   }
 `;
 
@@ -102,14 +116,15 @@ const PopularFollowerItem = () => {
     {popularFollowers.map((popular) => {
       return (
       <StyledFollowerItem key={popular.id}>
-        <StyledFollowerAvatar
-          image={popular.avatar}/>
         <StyledFollowerInfo>
-          <h6 className="popular-follower-name">{popular.name}</h6>
-          <div className="popular-follower-account fontSecondary">
-            @{popular.account}
+          <StyledFollowerAvatar image={popular.avatar}/>
+          <div className="popular-info-wrapper">
+            <h6 className="popular-follower-name">{popular.name}</h6>
+            <div className="popular-follower-account fontSecondary">
+              @{popular.account}</div>
           </div>
         </StyledFollowerInfo>
+        <div className="popular-button-wrapper">
         {currentMember.id !== popular.id && !popular.isFollowed ? (
           <FollowButton className="unfollowed" data-id={popular.id}
         onClick={(e) => {
@@ -132,6 +147,7 @@ const PopularFollowerItem = () => {
         }}>正在跟隨</FollowButton>
         )
         }
+        </div>
       </StyledFollowerItem>
       )
     })}
