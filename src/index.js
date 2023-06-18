4465fe28a93f04abf994c4b41b44091f1a59f3f6
Liam <liam67726978@gmail.com>
2023-06-19 06:23:28 +0800

    fix: routers

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import App from './App'
 import './reset.css'
 import './index.scss'
 
-// const basename = process.env.PUBLIC_URL
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
