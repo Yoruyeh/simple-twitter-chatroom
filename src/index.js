3634d2a981a5ecd7a4dfffadf45ed96624cb85b2
Yoruyeh <orchardsky31@gmail.com>
2023-06-18 22:00:26 +0800

    fix user reply page error and add base url for browser router

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App'
 import './reset.css'
 import './index.scss'
 
+const basename = process.env.PUBLIC_URL
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
      </BrowserRouter>
   </React.StrictMode>
