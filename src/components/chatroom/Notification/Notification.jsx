e310dd9480da029f861107feac9d7a9daf79a118
Yoruyeh <orchardsky31@gmail.com>
2023-06-20 21:20:40 +0800

    style: navbar and notification

--- src/components/chatroom/Notification/Notification.jsx
+++ src/components/chatroom/Notification/Notification.jsx
@@ -1,7 +1,58 @@
+import styles from './notification.module.scss'
 
 const Notification = () => {
   return (
-   <h1>123</h1>
+    <div className={styles.container}>
+      <header><h4>通知</h4></header>
+
+      <div className={styles.notiWrapper}>
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>John有新的推文通知</h6>
+          <p className={styles.content}>123112313213</p>
+        </div>
+
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>你的貼文有新的回覆</h6>
+          <p className={styles.content}>123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213</p>
+        </div>
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>John有新的推文通知</h6>
+          <p className={styles.content}>123112313213</p>
+        </div>
+
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>你的貼文有新的回覆</h6>
+          <p className={styles.content}>123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213</p>
+        </div>
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>John有新的推文通知</h6>
+          <p className={styles.content}>123112313213</p>
+        </div>
+
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>你的貼文有新的回覆</h6>
+          <p className={styles.content}>123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213</p>
+        </div>
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>John有新的推文通知</h6>
+          <p className={styles.content}>123112313213</p>
+        </div>
+
+        <div className={styles.notiItem}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <h6 className={styles.title}>你的貼文有新的回覆</h6>
+          <p className={styles.content}>123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213123112313213</p>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
