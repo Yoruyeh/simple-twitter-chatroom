e35348d2d816eec456f254c7442324b2c350c7bc
Yoruyeh <orchardsky31@gmail.com>
2023-06-20 23:11:44 +0800

    style: user message component

---
+++ src/components/chatroom/UserMessage/UserMessage.jsx
@@ -0,0 +1,40 @@
+import styles from './user.message.module.scss'
+
+const UserMessage = () => {
+  return (
+   <div className={styles.container}>
+      <header><h4>訊息</h4></header>
+
+      <div className={styles.messageWrapper}>
+        <div className={styles.messageItem}>
+          <div className={styles.info}>
+            <img src="" alt="avatar" className={styles.avatar} />
+            <div className={styles.text}>
+              <h6 className={styles.name}>Pizza Hut 
+              <span className={styles.account}> @pizzahut</span>
+              </h6>
+              <p className={styles.message}>12313454fd8f4dsf45d4f4ds56f413454fd8f4dsf45d4f4</p>
+             </div> 
+          </div>
+          <div className={styles.time}>6月31日</div>
+        </div>
+
+          <div className={styles.messageItem}>
+          <div className={styles.info}>
+            <img src="" alt="avatar" className={styles.avatar} />
+            <div className={styles.text}>
+              <h6 className={styles.name}>Pizza Hut 
+              <span className={styles.account}> @pizzahut</span>
+              </h6>
+              <p className={styles.message}>12313454fd8f4dsf45d4f4ds56f413454fd8ffd8f4dsf45d4f4ds56f413454fd8f4d4dsf45d4f4</p>
+             </div> 
+          </div>
+          <div className={styles.time}>6月31日</div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default UserMessage;
