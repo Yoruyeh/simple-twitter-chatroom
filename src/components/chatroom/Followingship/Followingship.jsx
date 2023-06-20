a53df62a3af1abb52ccfff32cd389ba80b374e7c
Yoruyeh <orchardsky31@gmail.com>
2023-06-20 21:56:45 +0800

    style: followship

--- src/components/chatroom/Followingship/Followingship.jsx
+++ src/components/chatroom/Followingship/Followingship.jsx
@@ -1,7 +1,23 @@
+import styles from './followingship.module.scss'
+import { FollowButton } from '../../common/button.styled'
 
 const Followingship = () => {
   return (
-   <h1>123</h1>
+   <div className={styles.container}>
+    <header><h4>跟隨誰</h4></header>
+    <div className={styles.followWrapper}>
+      <div className={styles.followItem}>
+        <div className={styles.info}>
+          <img src="" alt="avatar" className={styles.avatar} />
+          <div className="text">
+          <h6 className={styles.name}>Pizza Hut</h6>
+          <p className={styles.account}>@pizzahut</p>
+          </div>
+        </div>
+        <div className={styles.button}><FollowButton /></div>
+      </div>
+    </div>
+   </div>
   );
 }
 
